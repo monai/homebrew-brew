f558ac641f425fa140b3227b6a084a208890c13a
monai <monai@cure.lt>
2021-03-06 10:16:56 +0200

    Add formula libfragmentzip

---
+++ Formula/libfragmentzip.rb
@@ -0,0 +1,30 @@
+class Libfragmentzip < Formula
+  desc "A library allowing to download single files from a remote zip archive"
+  homepage "https://github.com/tihmstar/libfragmentzip"
+  url "https://github.com/tihmstar/libfragmentzip/archive/60.tar.gz"
+  sha256 "5038761c01b1d5be968e8dbc295cfb8c1189d130262d4833bac9d54cdd8c5b8c"
+  license "LGPL-3.0"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libgeneral" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "libzip"
+
+  def install
+    inreplace "configure.ac" do |s|
+      s.gsub! "m4_esyscmd([git rev-list --count HEAD | tr -d '\\n'])", "60"
+      s.gsub! "m4_esyscmd([git rev-parse HEAD | tr -d '\\n'])", "120447d0f410dffb49948fa155467fc5d91ca3c8"
+    end
+
+    system "./autogen.sh", "--prefix=#{prefix}"
+
+    system "make", "install"
+  end
+
+  test do
+    system "pkg-config", "libfragmentzip"
+  end
+end
