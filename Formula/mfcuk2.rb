f54a108f136120ff0a7b0bc69bf664af710636a9
monai <monai@cure.lt>
2021-03-07 11:21:37 +0200

    Add formula mfcuk2

---
+++ Formula/mfcuk2.rb
@@ -0,0 +1,30 @@
+class Mfcuk2 < Formula
+  url "https://github.com/xLinkOut/mfcuk.git", using: :git, revision: "2071bb05d6253a5a917105eea8dd993d6acb438d"
+  desc "MiFare Classic Universal toolKit"
+  homepage "https://github.com/xLinkOut/mfcuk"
+  license "GPL-2.0"
+  version "0.3.8-next"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libnfc"
+  depends_on "libusb-compat"
+
+  conflicts_with "mfcuk", because: "both install `mfcuk` binaries"
+
+  def install
+    inreplace "src/mfcuk.c", "clear_screen\(\);", ""
+
+    system "autoreconf", "--install", "--symlink"
+    system "./configure", "--prefix=#{prefix}"
+
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/mfcuk", "-h"
+  end
+end
