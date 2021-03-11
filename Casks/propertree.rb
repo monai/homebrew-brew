f35275b0fe4f7fc04f4d53ede1e034ca3f481061
monai <monai@cure.lt>
2021-03-11 17:16:04 +0200

    Use latest version

--- Casks/propertree.rb
+++ Casks/propertree.rb
@@ -1,16 +1,16 @@
 cask "propertree" do
-  version "b2bfde3067f7a717e994c49a55930a2ffb2e6acf"
-  sha256 "e2cdccdfef6f9ac6370c321d3fadf632620a76d5dbd5ec0dfcb8e38d0fd48860"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/corpnewt/ProperTree/archive/#{version}.zip"
+  url "https://github.com/corpnewt/ProperTree.git"
   name "ProperTree"
   desc "Cross platform GUI plist editor written in Python"
   homepage "https://github.com/corpnewt/ProperTree"
 
   preflight do
-    system "#{staged_path}/ProperTree-#{version}/Scripts/buildapp.command"
+    system "#{staged_path}/Scripts/buildapp.command"
   end
 
-  app "#{staged_path}/ProperTree-#{version}/ProperTree.app"
+  app "#{staged_path}/ProperTree.app"
   binary "#{appdir}/ProperTree.app/Contents/MacOS/ProperTree.command", target: "propertree"
 end
