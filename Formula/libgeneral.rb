2bf9197d7e54edb997b20d897447e108b2d7d607
monai <monai@cure.lt>
2021-08-26 11:00:18 +0300

    Upgrade libgeneral

--- Formula/libgeneral.rb
+++ Formula/libgeneral.rb
@@ -1,8 +1,8 @@
 class Libgeneral < Formula
   desc "General stuff for projects"
   homepage "https://github.com/tihmstar/libgeneral"
-  url "https://github.com/tihmstar/libgeneral/archive/54.tar.gz"
-  sha256 "e847ab7c6f4e099f536d6aff4e448adac11b3e3a0d124937eead42b641b7cf39"
+  url "https://github.com/tihmstar/libgeneral/archive/refs/tags/56.tar.gz"
+  sha256 "16ea189a414296a5867cfe4198bf548c941847807148260afb0cb0271750aa07"
   license "LGPL-2.1"
 
   depends_on "autoconf" => :build
@@ -12,7 +12,7 @@ class Libgeneral < Formula
 
   def install
     inreplace "configure.ac" do |s|
-      s.gsub! "m4_esyscmd([git rev-list --count HEAD | tr -d '\\n'])", "54"
+      s.gsub! "m4_esyscmd([git rev-list --count HEAD | tr -d '\\n'])", "56"
       s.gsub! "m4_esyscmd([git rev-parse HEAD | tr -d '\\n'])", "b04a27d0584c4c10c4b376325bb928c0ad12e285"
     end
 
