55c8bccf00202745b2a51916bfad1ce80f499735
monai <monai@cure.lt>
2022-04-22 12:45:46 +0300

    Add PDFium formula

---
+++ Formula/pdfium.rb
@@ -0,0 +1,29 @@
+class Pdfium < Formula
+  desc "Library for PDF manipulation and rendering"
+  homepage "https://github.com/bblanchon/pdfium-binaries"
+  url "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5010/pdfium-mac-x64.tgz"
+  sha256 "290b312764715d1a55339caa4bf1dc3561dc841186b60b3a118bc54576635797"
+  version "103.0.5010.0"
+  license "Apache-2.0"
+
+  def install
+    prefix.install Dir["*"]
+
+    (lib/"pkgconfig/#{name}.pc").write <<~EOS
+      prefix=#{prefix}
+      exec_prefix=${prefix}
+      libdir=${exec_prefix}/lib
+      includedir=${prefix}/include
+
+      Name: PDFium
+      Description: #{desc}
+      Version: #{version}
+      Libs: -L${libdir} -l#{name}
+      Cflags: -I${includedir}
+    EOS
+  end
+
+  test do
+    system "pkg-config", "pdfium"
+  end
+end
